79eafab2f3f9dc13ce0f4df86f6d2489da42934f
Juan Camilo Villada Gamboa <jncvg17@hotmail.com>
2018-02-11 19:47:10 -0500

    changes to index

--- docs/js/main.js
+++ docs/js/main.js
@@ -26,18 +26,6 @@ function main() {
 	})
 	//console.log(nagativeValue)
 	});
-	
-        $('.count').each(function () {
-        $(this).prop('Counter',0).animate({
-        Counter: $(this).text()
-        }, {
-        duration: 4000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
-            }
-         });
-    });
 
 
 }());
